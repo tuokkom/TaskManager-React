f0a781f62150a5ea86579554cd81fca11118d60c
miro tuokko <miro.tuokko@student.tut.fi>
2021-02-28 16:55:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState, useEffect } from 'react'
+import AddTask from './components/AddTask'
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      console.log(data)
+    }
+
+    fetchTasks()
+  }, [])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle reminder 
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder} : task)
+    )
+  }
+
+  return (
+    <div className="container">
+      <Header onAdd={ () => setShowAddTask(!showAddTask) } showAdd={showAddTask}/>
+      {showAddTask && 
+        <AddTask onAdd={addTask}/>
+      }
+      {tasks.length > 0 ? <Tasks 
+        tasks={tasks} 
+        onDelete={deleteTask} 
+        onToggle={toggleReminder}
+      /> : 'No tasks to show'}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const onClick = () => {
+        console.log('clicked')
+    }
+
+    return (
+        <header className='header'>
+            <h1 >{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Investment manager',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+/* css in js
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black',
+}
+*/
+
+export default Header
